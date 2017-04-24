75432a6d34515d1d57aa350c68f2cf17ed6178af
Fernando Fernandez <ffmp777@yahoo.com>
2017-04-24 11:12:40 -0600

    Show command to show when tasks are completed and added

---
+++ src/ShowCommand.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Acme;
+
+
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ShowCommand extends Command {
+	public function configure() {
+		$this->setName('show')
+			 ->setDescription('show all tasks');
+	}
+//* When we execute this command.
+	public function execute(InputInterface $input, OutputInterface $output) {
+//* Show all tasks.
+		$this->showTasks($output);
+	}
+}
+
+ ?>
